7543441afa5e1f8087b6c228e48ed9799536020a
Ksiusha <ksiusha5@gmail.com>
2015-06-18 19:15:57 +0300

    source

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp'),
+    $ = require('gulp-load-plugins')();
+
+gulp.task('sass', function () {
+    return gulp.src(['sass/**/*.sass'])
+        .pipe($.watch(['sass/**/*.sass']))
+        .pipe($.sourcemaps.init())
+        .pipe($.sass({
+            indentedSyntax: true,
+            errLogToConsole: true,
+            // outputStyle: 'compressed'
+        }))
+        .pipe($.autoprefixer('last 3 version'))
+        .pipe($.sourcemaps.write('.'))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('default', ['sass']);
